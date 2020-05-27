d073eb28be3c179324dbc4fbb9d78b6d0a8caf46
Kento Takano <kento2github@gmail.com>
2020-05-28 08:18:13 +0900

    Added bootstrap

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Todo;
+
 class TodosController extends Controller
 {
     public function index()
     {
-        return view('todos.index');
+        return view('todos.index')->with('todos', Todo::all());
     }
 }
