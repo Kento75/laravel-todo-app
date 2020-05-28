c1203210a9a15df31d92973b0d6f7ffcbb57afea
Kento Takano <kento2github@gmail.com>
2020-05-28 09:25:57 +0900

    Added show detail page

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -8,8 +8,15 @@ use App\Todo;
 
 class TodosController extends Controller
 {
+    // 記事一覧トップ画面
     public function index()
     {
         return view('todos.index')->with('todos', Todo::all());
+    }
+
+    // 記事詳細画面
+    public function show($todoId)
+    {
+        return view('todos.show')->with('todo', Todo::find($todoId));
     }
 }
