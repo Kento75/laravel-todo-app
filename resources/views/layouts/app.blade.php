a90e31d3a64ce8d331546483b72b3e75448e43c9
Kento Takano <kento2github@gmail.com>
2020-05-29 08:17:29 +0900

    Added layouts header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" crossorigin="anonymous">
-    <title>Todos</title>
+    <title>
+        @yield('title')
+    </title>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
