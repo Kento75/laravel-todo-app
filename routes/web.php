72c7eb1fbd2516d31e659aa19b1da73e24c51204
Kento Takano <kento2github@gmail.com>
2020-06-01 08:49:49 +0900

    Added delete routes

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,16 @@ Route::get('/', function () {
 Route::get('todos', 'TodosController@index');
 // 記事詳細ページ
 Route::get('todos/{todo}', 'TodosController@show');
+
 // 記事投稿フォームページ
 Route::get('new-todos', 'TodosController@create');
 // 記事投稿
 Route::post('store-todos', 'TodosController@store');
+
 // 記事更新フォームページ
 Route::get('todos/{todo}/edit', 'TodosController@edit');
 // 記事更新
 Route::post('todos/{todo}/update-todos', 'TodosController@update');
+
+// 記事削除
+Route::get('todos/{todo}/delete', 'TodosController@destroy');
